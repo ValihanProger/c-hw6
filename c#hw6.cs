8da16dc0bac37f804b75c344a4e1b2be34bf7477
Valihan <valihanburhanov@yandex.ru>
2023-01-30 10:45:35 +0300

    task2 complete

--- c#hw6.cs
+++ c#hw6.cs
@@ -1,46 +1,23 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-// 0, 7, 8, -2, -2 -> 2
-// 1, -7, 567, 89, 223-> 3
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
-// int[] ConverterToBinary(int num)
-// {
-//     int[] result = new int [num];
-//     int i = 0;
-//     while (num > 0)
-//     {
-//         result[i] = num%2;
-//         Console.WriteLine(result);
-//         num = num/2;
-//         i = i + 1;
-//     }
-
-//     foreach (var item in result.Reverse())
-//     {
-//         Console.Write(item);
-//     }
-//     return result;
-// }
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 
-// Console.Write("Enter a number M: ");
-// int M = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine($"   {num} -> {ConverterToBinary(num)}");
-
-int count = 0;
-int CntZeroes()
-{
-    Console.Write("Введите элементы(через пробел): ");
-    int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);    
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (arr[i] > 0)
-        {
-            count++;
-        }
-    }
-    Console.WriteLine($"Кол-во элементов > 0: {count}");
-    return count;
-
-
-}
-CntZeroes();
+Console.Write("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+ 
+ 
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
